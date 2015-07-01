0977a2662034653a8b77236ea8082f6cb7d7fafc
thomascjohnson <thomascjohnson@gmail.com>
2015-07-01 13:59:24 -0400

    Update ft.connect.R

--- R/ft.connect.R
+++ R/ft.connect.R
@@ -1,10 +1,10 @@
 ft.connect <- function(client_id,
                        client_secret,
-                       scopes = c("https://www.googleapis.com/auth/fusiontables",
+                       api_scopes = c("https://www.googleapis.com/auth/fusiontables",
                                   "https://www.googleapis.com/auth/fusiontables.readonly")) {
   require(httr)
   require(rjson)
   app <- oauth_app("google", client_id, client_secret)
-  auth_key <- oauth2.0_token(oauth_endpoints("google"), app, scopes)
+  auth_key <- oauth2.0_token(oauth_endpoints("google"), app, api_scopes)
   return(auth_key)
 }
