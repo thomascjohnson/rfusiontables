4f5530d64bda555f7846e393301bdc079819cc39
thomascjohnson <thomascjohnson@gmail.com>
2015-07-01 10:50:32 -0400

    Create ft.sqlquery.R

---
+++ R/ft.sqlquery.R
@@ -0,0 +1,10 @@
+ft.showtables <- function(auth, sqlquery) {
+  url <- "https://www.googleapis.com/fusiontables/v2/query"
+  params <- list(sql = sqlquery)
+  
+  result <- POST(url, config(token=auth), query = params)
+  
+  contents <- content(result)
+  
+  return(contents)
+}
